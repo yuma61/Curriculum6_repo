ce3eb54b3214e5d499848fe2dec7bc1369a002b0
ym33522995 <yuma.m530@gmail.com>
2024-12-07 20:57:37 +0900

    Curriculum07_2 "Created view for blog page. Changed the route for the / access"

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <title>Blog</title>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    </head>
+    <body>
+        <h1>Blog Name</h1>
+        <div class='posts'>
+            <div class='post'>
+                <h2 class='title'>Title</h2>
+                <p class='body'>This is a sample body.</p>
+            </div>
+        </div>
+    </body>
+</html>
+
+
--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use App\Http\Controllers\PostController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('posts.index');
 });
 
 Route::get('/posts', [PostController::class, 'index']);   
